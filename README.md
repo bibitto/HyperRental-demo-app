13dff5f191e714cff770545a527945ae074e268c
bibitto <bibitto.crypto@gmail.com>
2023-06-12 14:48:44 +0900

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Hyper Rental
+
+## overview
+
+A peer-to-peer collateral-free rental protocol which utilizing token bound account to allow true ownership of the rented NFT for the borrower while suppressing the liquidation risk for the lender.
+
+## what is new
+
+- All accounts such as EOAs and contract wallets can be true owner of rented assets
+- Supports free collateral lending (The rental fee remains on renter's wallet for the duration of the rental period)
+- Renters can transfer the ownership of rented assets freely
+- All Assets like ERC-20, ERC-721, ERC-1155 and ERC-3525 are suppported
---
+++ pages/index.tsx
@@ -0,0 +1,67 @@
+import type { NextPage } from "next";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+/**
+ * Landing page with a simple gradient background and a hero asset.
+ * Free to customize as you see fit.
+ */
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.hero}>
+          <div className={styles.heroBackground}>
+            <div className={styles.heroBackgroundInner}>
+              <Image
+                src="/hero-gradient.png"
+                width={1390}
+                height={1390}
+                alt="Background gradient from red to blue"
+                quality={100}
+                className={styles.gradient}
+              />
+            </div>
+          </div>
+          <div className={styles.heroBodyContainer}>
+            <div className={styles.heroBody}>
+              <h1 className={styles.heroTitle}>
+                <span className={styles.heroTitleGradient}>Welcome 👋</span>
+                <br />
+                Hyper Rental
+              </h1>
+              <p className={styles.heroSubtitle}>
+                <Link
+                  className={styles.link}
+                  href="https://github.com/bibitto/Hyper-Rental"
+                  target="_blank"
+                >
+                  Hyper Rental
+                </Link>{" "}
+                is a peer-to-peer collateral-free rental protocol which
+                utilizing token bound account to allow true ownership of the
+                rented NFT for the renters.
+              </p>
+
+              <div className={styles.heroCtaContainer}>
+                <Link className={styles.heroCta} href="/buy">
+                  Get Started
+                </Link>
+                <Link
+                  className={styles.secondaryCta}
+                  href="https://github.com/thirdweb-example/marketplace-v3"
+                  target="_blank"
+                >
+                  GitHub
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
