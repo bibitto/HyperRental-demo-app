0d88fc0c6e902c1265da08b773ce5c5f97289cc1
bibitto <bibitto.crypto@gmail.com>
2023-06-20 12:41:03 +0900

    update NFT Grid component

--- components/NFT/NFTGrid.tsx
+++ components/NFT/NFTGrid.tsx
@@ -1,52 +1,44 @@
 import type { NFT as NFTType } from "@thirdweb-dev/sdk";
 import Link from "next/link";
 import React from "react";
-import { NFT_COLLECTION_ADDRESS } from "../../const/contractAddresses";
 import Skeleton from "../Skeleton/Skeleton";
 import NFT from "./NFT";
 import styles from "../../styles/Buy.module.css";
+import { RentalPackMetadata } from "../../util/type";
 
 type Props = {
   isLoading: boolean;
-  data: NFTType[] | undefined;
-  overrideOnclickBehavior?: (nft: NFTType) => void;
+  data: RentalPackMetadata[] | undefined;
+  // overrideOnclickBehavior?: (nft: NFTType) => void;
   emptyText?: string;
+  handleSelect: (r: RentalPackMetadata) => void;
 };
 
 export default function NFTGrid({
   isLoading,
   data,
-  overrideOnclickBehavior,
+  // overrideOnclickBehavior,
   emptyText = "No NFTs found for this collection.",
+  handleSelect,
 }: Props) {
   return (
     <div className={styles.nftGridContainer}>
-      {isLoading ? (
+      {!data ? (
         [...Array(20)].map((_, index) => (
           <div key={index} className={styles.nftContainer}>
             <Skeleton key={index} width={"100%"} height="312px" />
           </div>
         ))
       ) : data && data.length > 0 ? (
-        data.map((nft) =>
-          !overrideOnclickBehavior ? (
-            <Link
-              href={`/token/${NFT_COLLECTION_ADDRESS}/${nft.metadata.id}`}
-              key={nft.metadata.id}
-              className={styles.nftContainer}
-            >
-              <NFT nft={nft} />
-            </Link>
-          ) : (
-            <div
-              key={nft.metadata.id}
-              className={styles.nftContainer}
-              onClick={() => overrideOnclickBehavior(nft)}
-            >
-              <NFT nft={nft} />
-            </div>
-          )
-        )
+        data.map((nft, i) => (
+          <div
+            key={i}
+            className={styles.nftContainer}
+            onClick={() => handleSelect(nft)}
+          >
+            <NFT rentalPack={nft} />
+          </div>
+        ))
       ) : (
         <p>{emptyText}</p>
       )}
