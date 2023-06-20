979123e4f9f5e9bdc648ac8b8b2e412680501194
bibitto <bibitto.crypto@gmail.com>
2023-06-20 12:38:36 +0900

    add: const file

---
+++ util/const.ts
@@ -0,0 +1,9 @@
+import { Mumbai } from "@thirdweb-dev/chains";
+export const NETWORK = Mumbai;
+
+export const MUMBAI_CHAIN_ID = "0x13881";
+
+export const HYPER_RENTAL_ADDRESS =
+  "0x832c123B4c56A899d653343F76797C3270ab2049";
+
+export const RENTAL_PACK_ADDRESS = "0xEf5F367Ff668b18A50D9C31bFe961bd51E6C22F9";
